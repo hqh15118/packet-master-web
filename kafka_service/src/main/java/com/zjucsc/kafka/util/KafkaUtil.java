cb5e7e6a0d5d34a082d0b245c505ec3a2d409a7b
hongqianhui <hong19961218@qq.com>
2019-05-29 09:44:49 +0800

    add lua engine

--- kafka_service/src/main/java/com/zjucsc/kafka/util/KafkaUtil.java
+++ kafka_service/src/main/java/com/zjucsc/kafka/util/KafkaUtil.java
@@ -5,10 +5,10 @@ import java.util.Properties;
 public class KafkaUtil {
 
     public static Properties getProducerProperties(){
-
+        return null;
     }
 
     public static Properties getConsumerProperties(){
-
+        return null;
     }
 }
--- src/main/java/com/zjucsc/application/system/service/hession_config/ArtHistoryHessianConfig.java
+++ src/main/java/com/zjucsc/application/system/service/hession_config/ArtHistoryHessianConfig.java
@@ -1,28 +1,24 @@
 package com.zjucsc.application.system.service.hession_config;
 
 
-import com.zjucsc.application.system.service.iservice.IArtHistoryData;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.remoting.caucho.HessianProxyFactoryBean;
 
 @Configuration
-@ConfigurationProperties(prefix = "hessian_service_config.hessian_url")
+@ConfigurationProperties(prefix = "hessian-service-config")
 public class ArtHistoryHessianConfig {
 
-    @Value("art_history_data")
-    private String artHistoryData;
-
-
-    @Bean
-    public HessianProxyFactoryBean artHistoryHessianBean(){
-        HessianProxyFactoryBean factory = new HessianProxyFactoryBean();
-        factory.setServiceUrl(artHistoryData);
-        factory.setServiceInterface(IArtHistoryData.class);
-        return factory;
-    }
+//    @Value("art-history-data")
+//    private String artHistoryData;
+//
+//
+//    @Bean
+//    public HessianProxyFactoryBean artHistoryHessianBean(){
+//        HessianProxyFactoryBean factory = new HessianProxyFactoryBean();
+//        factory.setServiceUrl(artHistoryData);
+//        factory.setServiceInterface(IArtHistoryData.class);
+//        return factory;
+//    }
 
 
 }
