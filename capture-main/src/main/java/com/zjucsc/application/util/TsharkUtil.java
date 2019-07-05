16b8f615762ec8895dc9de434a5cf6fa3b290d83
hongqianhui <hong19961218@qq.com>
2019-07-05 13:49:20 +0800

    add tshark plugin auto-check

--- capture-main/src/main/java/com/zjucsc/application/util/TsharkUtil.java
+++ capture-main/src/main/java/com/zjucsc/application/util/TsharkUtil.java
@@ -1,17 +1,22 @@
 package com.zjucsc.application.util;
 
+import java.io.*;
+
 public class TsharkUtil {
 
-    public static String getTsharkPath(){
-            return null;
+    private static String tsharkPath = null;
+    public static void setTsharkPath(String path){
+        tsharkPath = path;
     }
-
+    private static String getTsharkPath(){
+            return tsharkPath;
+    }
 
     public static String checkTsharkValid(){
         String str = System.getenv("PATH");
         String[] allPathVar = null;
-        if (str.contains(":")){
-            allPathVar = str.split(":");
+        if (str.contains(";")){
+            allPathVar = str.split(";");
         }
         assert allPathVar!=null;
         for (String var : allPathVar) {
@@ -20,5 +25,25 @@ public class TsharkUtil {
             }
         }
         return null;
+    }
+
+    public static void addTsharkPlugin() throws IOException {
+        String pluginPath = getTsharkPath() + "\\plugins\\mywireshark_plugin.lua";
+        File file = new File(pluginPath);
+        if (!file.exists()){
+            if(file.createNewFile()){
+                System.out.println("未检测到tshark【mywireshark_plugin.lua】插件，已自动创建");
+            }
+            InputStream is = TsharkUtil.class.getResourceAsStream("/mywireshark_plugin.lua");
+            int length = is.available();
+            byte[] data = new byte[length];
+            int readLength = is.read(data);
+            assert readLength == length;
+            OutputStream os = new FileOutputStream(file);
+            os.write(data);
+            os.flush();
+        }else{
+            System.out.println("检测到tshark插件>>");
+        }
     }
 }
