0854c78b47fa1d085c8f5d82592f79f44625e26b
FreedomZPF <384527940@qq.com>
2019-07-23 11:18:34 +0800

    s7pniomodbus_artdecode

---
+++ art-decode/src/main/java/com/zjucsc/art_decode/s7comm/DBclass.java
@@ -0,0 +1,42 @@
+package com.zjucsc.art_decode.s7comm;
+
+public class DBclass {
+
+    private int dbnum;
+    private int byteoffset;
+    private int bitoffset;
+    private int length;
+
+    public int getBitoffset() {
+        return bitoffset;
+    }
+
+    public int getByteoffset() {
+        return byteoffset;
+    }
+
+    public int getDbnum() {
+        return dbnum;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setBitoffset(int bitoffset) {
+        this.bitoffset = bitoffset;
+    }
+
+    public void setByteoffset(int byteoffset) {
+
+        this.byteoffset = byteoffset;
+    }
+
+    public void setDbnum(int dbnum) {
+        this.dbnum = dbnum;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+}
